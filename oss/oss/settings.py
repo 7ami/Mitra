4a7c07f73868cdeee58a7984c00896cbed12a042
amir <amir.ranabhat@gmail.com>
2020-01-20 20:20:29 +0545

    Accomodations well prepared

--- oss/oss/settings.py
+++ oss/oss/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'oss.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['oss/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
